7f4e8436511ab5416a9e4bff5d61b7128e2e6da8
simoesnvc <joao.familiasimoes@gmail.com>
2025-10-21 14:24:47 +0000

    just missing textures for wall

--- game/render.c
+++ game/render.c
@@ -12,11 +12,15 @@
 
 #include "../cubo3d.h"
 
-static void	clear_image(t_img *img)
+static void	clear_image(t_game *game, t_img *img)
 {
 	int	x;
 	int	y;
+	int		ceiling_color;
+	int		floor_color;
 
+	ceiling_color = (game->C_R << 16) | (game->C_G << 8) | game->C_B;
+	floor_color = (game->F_R << 16) | (game->F_G << 8) | game->F_B;
 	y = -1;
 	while (++y < HEIGHT)
 	{
@@ -24,9 +28,9 @@ static void	clear_image(t_img *img)
 		while (++x < WIDTH)
 		{
 			if (y < HEIGHT / 2)
-				my_mlx_pixel_put(img, x, y, 0x0000FF);
+				my_mlx_pixel_put(img, x, y, ceiling_color);
 			else
-				my_mlx_pixel_put(img, x, y, 0x000000);
+				my_mlx_pixel_put(img, x, y, floor_color);
 		}
 	}
 }
@@ -51,7 +55,7 @@ static void	cast_all_rays(t_game *game)
 
 int	render(t_game *game)
 {
-	clear_image(&game->screen);
+	clear_image(game, &game->screen);
 	cast_all_rays(game);
 	mlx_put_image_to_window(game->mlx, game->win, game->screen.img, 0, 0);
 	return (0);
